49aa2a7627f454475cf01fab88eed99bbb9edb00
Jose_Enrique <jeregalon@gmail.com>
2025-09-16 16:59:18 -0400

    new way to build the tree

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,30 +1,34 @@
+from Vertex import Vertex
+
 class BinaryTree:
-    def __init__(self, root, left_child, righ_child):
-        self._root = root
-        self._left_child = left_child
-        self._right_child = righ_child
-    
-    @property
-    def root(self):
-        return self._root
-    
-    @property
-    def left_child(self):
-        return self._left_child
-    
-    @property
-    def right_child(self):
-        return self._right_child
-    
-    @root.setter
-    def root(self, value):
-        self._root = value
+    def __init__(self, elements):
+        self._elements = elements
+        self._root = Vertex(None, None, None)
 
-    @left_child.setter
-    def left_child(self, value):
-        self._left_child = value
+    def build(self):
+        elements = self._elements.sort()
+        leftSubtree = []
+        rightSubtree = []
+        leng = len(elements)
+        if leng > 2:
+            if leng % 2 == 0:
+                half = leng / 2 - 1
+            else:
+                half = leng // 2
+            leftSubtree = BinaryTree(elements[:half-1])
+            root = elements[half]
+            rightSubtree = BinaryTree(elements[half+1:])
+        elif leng == 2:
+            root = elements[0]
+            rightSubtree = elements[1]
+        elif leng == 1:
+            root = elements[0]
+        else:
+            root = None
 
-    @right_child.setter
-    def right_child(self, value):
-        self._right_child = value
-    
+        newVertex = Vertex(root, None, None)
+        newVertex.left_child = leftSubtree.build()
+        newVertex.right_child = rightSubtree.build()
+        
+        return newVertex
+
--- TreeSearching.py
+++ TreeSearching.py
@@ -14,36 +14,14 @@ def TreeSearching(T, value):
 # Ejemplo:
 
 # Construyo el árbol
-
-mainTree = BinaryTree('FOUR', None, None)
-andT = BinaryTree('AND', None, None)
-scoreT = BinaryTree('SCORE', None, None)
-agoT = BinaryTree('AGO', None, None)
-sevenT = BinaryTree('SEVEN', None, None)
-ourT = BinaryTree('OUR', None, None)
-yearsT = BinaryTree('YEARS', None, None)
-broughtT = BinaryTree('BROUGHT', None, None)
-forefathersT = BinaryTree('FOREFATHERS', None, None)
-forthT = BinaryTree('FORTH', None, None)
-
-broughtT.right_child = forthT
-ourT.left_child = broughtT
-agoT.right_child = ourT
-andT.left_child = agoT
-mainTree.left_child = andT
-
-yearsT.left_child = forefathersT
-sevenT.right_child = yearsT
-scoreT.right_child = sevenT
-mainTree.right_child = scoreT
+mainTree = BinaryTree("OLD PROGRAMMERS NEVER DIE, THEY JUST LOSE THEIR MEMORIES".split())
 
 # Probando la función
-
-print(TreeSearching(mainTree, 'FOREFATHERS'))   # True
-print(TreeSearching(mainTree, 'FOREFATHER'))    # False
-print(TreeSearching(mainTree, 'AND'))   # True
-print(TreeSearching(mainTree, 'BRO'))   # False
-print(TreeSearching(mainTree, 7))       # False
+print(TreeSearching(mainTree, 'FOREFATHERS'))   # 
+print(TreeSearching(mainTree, 'PROGRAMMERS'))    # 
+print(TreeSearching(mainTree, 'AND'))   # 
+print(TreeSearching(mainTree, 'DIE'))   # 
+print(TreeSearching(mainTree, 7))       # 
 
 
 
